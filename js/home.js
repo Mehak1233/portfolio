18eed937d837c11db593e1caa92a16745f8e8715
Mehak1233 <Mehak.Mahajan@georgebrown.ca>
2022-07-01 14:03:04 -0400

    first commit

---
+++ js/home.js
@@ -0,0 +1,30 @@
+
+
+// menu show
+const showMenu = (toggleId , navId) => {
+    const toggle = document.getElementById(toggleId);
+    nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            console.log("eneter")
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// Active And Remove Menu
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    //active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+  
+    // remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
